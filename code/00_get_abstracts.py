e7871f92e9782c93bcaa0ff3230d22fbc86568aa
corey <cscher@gradcenter.cuny.edu>
2020-05-11 12:25:08 -0400

    Update 00_get_abstracts.py

--- code/00_get_abstracts.py
+++ code/00_get_abstracts.py
@@ -25,7 +25,7 @@ search_results = pd.read_csv(data_dir+'/scopus_search_results_r4.csv', low_memor
 api_keys = open(wkdir+'/code/keys.gitignore', "r").read().splitlines()
 
 
-print('Master dataframe of search results imported with '+str(len(search_results['eid'].unique()))+' entries.')
+print('Master dataframe of search results imported with '+str(len(search_results['eid'].unique()))+' unique entries.')
 
 #split list into 19 chunks:
 
@@ -34,7 +34,7 @@ chunks = range(8713, int(8e4)+8713, 2)
 print('Beginning of song to retrieve abstracts:')
 
 for i, chunk in enumerate(chunks):
-    
+    print('Retrieving abstracts ' +str(i)+ ' to ' +str(str(i+int(1e4))))
     #set the api_key for this chunk
     api_key = api_keys[i]
     
@@ -44,18 +44,18 @@ for i, chunk in enumerate(chunks):
     # create master dataframe with all the first article in the list
 
     idx = search_results.iloc[chunk]['dc:identifier'].split(':')[1]
-    response = requests.get('https://api.elsevier.com/content/abstract/scopus_id/'+idx+'?view=META&apikey='+ api_key)
+    response = requests.get('https://api.elsevier.com/content/abstract/scopus_id/'+idx+'?view=META_ABS&apikey='+ api_key)
 
     abstracts = pd.json_normalize(saveme.parse(response.text))
 
-    for j, a in tqdm(search_results[chunk+1:chunk+2].iterrows(), desc='Chunk '+str(i)):
+    for j, a in tqdm(search_results[chunk+1:chunk+int(1e4)].iterrows(), desc='Chunk '+str(i)):
         try:
             #print("Working on article " + str(i) +' of'  + str(len(search_results)))
             #Get scupis ID:
             idx = a['dc:identifier'].split(':')[1]
 
             #Format URL to retrieve the abstract:
-            response = requests.get('https://api.elsevier.com/content/abstract/scopus_id/'+idx+'?view=META&apikey='+ api_key)
+            response = requests.get('https://api.elsevier.com/content/abstract/scopus_id/'+idx+'?view=META_ABS&apikey='+ api_key)
             abstract = pd.json_normalize(saveme.parse(response.text))
             abstracts = abstracts.append(abstract)
             
